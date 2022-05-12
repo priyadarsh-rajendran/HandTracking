6d1d648ae5c22c34df8b1aaf6cd3802c76794f98
priyadarsh-rajendran <36024292+priyadarsh-rajendran@users.noreply.github.com>
2022-05-12 13:34:02 +0530

    Add files via upload

---
+++ HandDetection.py
@@ -0,0 +1,12 @@
+#cam, handlandmarks and handconnections code 
+import cv2 
+import mediapipe as mp
+
+# Import all the dependencies and functions of OpenCV and Mediapipe
+
+# Write code here for while loop to process and recognise hands 
+
+
+
+
+
